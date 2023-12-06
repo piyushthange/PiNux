71ada9e2513b4b149f015427ec06794a27305fd9
Piyush Thange <pthange19@gmail.com>
2023-12-06 18:32:06 +0530

    Addition to bootloader
    Signed-off-by: Piyush Thange <pthange19@gmail.com>

--- bootloader/boot.asm
+++ bootloader/boot.asm
@@ -1,7 +1,24 @@
-ORG 0x7c00
+ORG 0
 BITS 16
+_start:
+	jmp short start
+	nop
+
+times 33 db 0
 
 start:
+	jmp 0x7c0:step2
+
+step2:
+	cli ; Clear Interrupts
+	mov ax, 0x7c0
+	mov ds, ax
+	mov es, ax
+	mov ax, 0x00
+	mov ss, ax
+	mov sp, 0x7c00
+	sti ; Enable Interrupts
+
 	mov si, message
 	call print
 	jmp $
