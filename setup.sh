e87e8c9d7b80c362161726ec17aae7c76c585aab
Piyush Thange <pthange19@gmail.com>
2023-12-14 21:39:20 +0530

    Cross compiler tools added in setup
    Signed-off-by: Piyush Thange <pthange19@gmail.com>

--- setup.sh
+++ setup.sh
@@ -2,3 +2,9 @@
 
 sudo apt update
 sudo apt install qemu-system nasm gdb
+
+# Cross Compilation tools
+
+sudo apt install build-essential flex bison \
+		 libgmp3-dev libmpc-dev libmpfr-dev \
+		 texinfo libcloog-isl-dev libisl-dev
