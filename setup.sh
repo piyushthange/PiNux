defe86f3040aba899c8d12370e165602e1cf5107
Piyush Thange <pthange19@gmail.com>
2024-01-06 23:08:51 +0530

    bless package a hex editor
    Signed-off-by: Piyush Thange <pthange19@gmail.com>

--- setup.sh
+++ setup.sh
@@ -7,4 +7,5 @@ sudo apt install qemu-system nasm gdb
 
 sudo apt install build-essential flex bison \
 		 libgmp3-dev libmpc-dev libmpfr-dev \
-		 texinfo libcloog-isl-dev libisl-dev
+		 texinfo libcloog-isl-dev libisl-dev \
+		 bless \ #hex editor
