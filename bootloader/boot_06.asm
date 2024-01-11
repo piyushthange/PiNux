4b12cbcb98cc297952c897ba8174cb5e34d3c230
Piyush Thange <pthange19@gmail.com>
2024-01-11 17:28:06 +0530

    boot_06 for Protected mode
    Signed-off-by: Piyush Thange <pthange19@gmail.com>

---
+++ bootloader/boot_06.asm
@@ -0,0 +1,70 @@
+ORG 0x7c00 ; changing the orgin to 0
+BITS 16 ; 16 bits arch
+
+CODE_SEG equ gdt_code - gdt_start
+DATA_SEG equ gdt_data - gdt_start
+
+_start: ; This will jump to the first 3 bytes 
+	jmp short start
+	nop
+
+times 33 db 0 ; this will turn 1st 33 bytes to 0 as described in BPB(https://wiki.osdev.org/FAT#BPB_.28BIOS_Parameter_Block.29)
+
+start:
+	jmp 0:step2
+
+step2:
+	cli ; Clear Interrupts
+	mov ax, 0x00
+	mov ds, ax
+	mov es, ax
+	mov ss, ax
+	mov sp, 0x7c00
+	sti ; enables Interrupts
+
+	jmp $
+
+.load_protected:
+	cli
+	lgdt[gdt_descriptor]
+	mov eax, cr0
+	or eax, 0x1
+	mov cr0, eax
+	jmp CODE_SEG:load32
+
+;GDT
+gtd_start:
+gtd_null:
+	dd 0x0 ;64 bits of null descriptor
+	dd 0x0
+
+; offset 0x8
+gdt_code:	  ; code segment(CS) should point to this
+	dw 0xffff ;Segment limit first 0-15 bits
+	dw 0	  ; Base first 0-15 bits
+	db 0 	  ; Base 16-23 bits
+	db 0x9a	  ; Access byte
+	db 11001111b ; HIGH 4 bit flags & low 4 bit flags
+	db 0	   ; base 24-31 bits
+
+;offset 0x10
+gdt_data:	  ; DS, SS, ES, FS, GS
+	dw 0xffff ;Segment limit first 0-15 bits
+	dw 0	  ; Base first 0-15 bits
+	db 0 	  ; Base 16-23 bits
+	db 0x92	  ; Access byte
+	db 11001111b ; HIGH 4 bit flags & low 4 bit flags
+	db 0	   ; base 24-31 bits
+
+gdt_end:
+
+gdt_descriptor:
+	dw gdt_end - gdt_start - 1
+	dd gdt_start
+
+[BITS 32]
+load32:
+	jmp $
+
+times 510-($ - $$) db 0
+dw 0xAA55 ; little endian
