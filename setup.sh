714bed185a43f805e90c5bfeee72cb859d1765d7
Piyush Thange <pthange19@gmail.com>
2024-01-19 17:04:53 +0530

    Dependencies descriptiong
    Signed-off-by: Piyush Thange <pthange19@gmail.com>

--- setup.sh
+++ setup.sh
@@ -5,7 +5,19 @@ sudo apt install qemu-system nasm gdb
 
 # Cross Compilation tools
 
-sudo apt install build-essential flex bison \
-		 libgmp3-dev libmpc-dev libmpfr-dev \
-		 texinfo libcloog-isl-dev libisl-dev \
-		 bless \ #hex editor
+#flex - Fast lexical analyzer generator
+#bison - general purpose parser generator that converts an annotated context-free grammar into a deterministic LR
+# gmp3 - Arithmetic lib
+#MPFR - Multiple Precision Floating-Point Reliable Library
+#texinfo - produces o/p in multiple formats
+#CLOOG - Code Generation Library for Optimized Code Generation
+# bless - hex editor
+
+sudo apt install -y build-essential \
+		 flex \
+		 bison \
+		 libgmp3-dev \
+		 libmpc-dev libmpfr-dev \
+		 texinfo \
+		 libisl-dev \
+		 bless 
